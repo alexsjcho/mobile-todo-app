4d20a3cdd19757918ce8478f7889a8faf6a709a1
Alex Cho <alexsjcho@gmail.com>
2018-11-26 07:14:46 -0800

    finished practice project

--- js/main.js
+++ js/main.js
@@ -15,65 +15,95 @@ $(document).ready(function () {
         $('#todos').listview('refresh');
     }
 
-    //Add Todo
-    $('#add_form').submit(function () {
-        //Get submitted values
-        var todo_name = $('#todo_name').val();
-        var todo_date = $('#todo_date').val();
+   //Add Todo
+$('#add_form').submit(function(){
+	//Get submitted values
+	var todo_name = $('#todo_name').val();
+	var todo_date = $('#todo_date').val();
+	
+	//Simple field validation
+	if(todo_name == ''){
+		alert('Please give the todo a name');
+	} else if(todo_date == ''){
+		alert('Please add a date');
+	} else {
+		var todos = JSON.parse(localStorage.getItem('todos'));
+		//Check todos
+		if(todos == null){
+			todos = [];
+		}
+		//Create array with new todo
+		var new_todo = {
+			"todo_name": todo_name,
+			"todo_date": todo_date
+		}
+		todos.push(new_todo);
+		localStorage.setItem('todos',JSON.stringify(todos));
+	}
+});
 
-        //Simple field validation
-        if (todo_name == '') {
-            alert('Please give the todo a name');
-        } else if (todo_date == '') {
-            alert('Please add a date');
-        } else {
-            var todos = JSON.parse(localStorage.getItem('todos'));
-            //Check todos
-            if (todos == null) {
-                todos = [];
-            }
-            //Create array with new todo
-            var new_todo = {
-                "todo_name": todo_name,
-                "todo_date": todo_date
-            }
-            todos.push(new_todo);
-            localStorage.setItem('todos', JSON.stringify(todos));
-        }
+	//Edit Todo
+	$('#edit_form').submit(function(){
+		currentTodoName = localStorage.getItem('currentTodoName');
+		currentTodoDate = localStorage.getItem('currentTodoDate');
+		//Loop through todos
+		for(var i=0; i < todoList.length; i++){
+			if(todoList[i].todo_name == currentTodoName){
+				todoList.splice(i,1);
+			}
+			localStorage.setItem('todos',JSON.stringify(todoList));
+		}	
+		
+		//Create a new todo
+		var todo_name_edit = $('#todo_name_edit').val();
+		var todo_date_edit = $('#todo_date_edit').val();
+		
+		var todos = JSON.parse(localStorage.getItem('todos'));
+		
+		//Create array with new values
+		var update_todo = {
+			"todo_name": todo_name_edit,
+			"todo_date": todo_date_edit
+		};
+		
+		todos.push(update_todo);
+		localStorage.setItem('todos',JSON.stringify(todos));
+	});
 
-        //Delete Todo
-        $('#edit_form').on('click', '#delete', function () {
-            currentTodoName = localStorage.getItem('currentTodoName');
-            currentTodoDate = localStorage.getItem('currentTodoDate');
-            //Loop through todos
-            for (var i = 0; i < todoList.length; i++) {
-                if (todoList[i].todo_name == currentTodoName) {
-                    todoList.splice(i, 1);
-                }
-                localStorage.setItem('todos', JSON.stringify(todoList));
-            }
-            //Close and go home
-            $.mobile.changePage($('#home'), 'pop');
-        });
+	//Delete Todo
+	$('#edit_form').on('click','#delete',function(){
+		currentTodoName = localStorage.getItem('currentTodoName');
+		currentTodoDate = localStorage.getItem('currentTodoDate');
+		//Loop through todos
+		for(var i=0; i < todoList.length; i++){
+			if(todoList[i].todo_name == currentTodoName){
+				todoList.splice(i,1);
+			}
+			localStorage.setItem('todos',JSON.stringify(todoList));
+		}	
+		//Close and go home
+		$.mobile.changePage($('#home'),'pop');
+	});
 
-        $('#todos').on('click', '#todo_link', function () {
-            localStorage.setItem('currentTodoName', $(this).data('todo_name'));
-            localStorage.setItem('currentTodoDate', $(this).data('todo_date'));
-        });
+	$('#todos').on('click','#todo_link',function(){
+		localStorage.setItem('currentTodoName',$(this).data('todo_name'));
+		localStorage.setItem('currentTodoDate',$(this).data('todo_date'));
+	});
+	
+	//Insert current data into edit form
+	$(document).on('pageshow','#edit',function(){
+		currentTodoName = localStorage.getItem('currentTodoName');
+		currentTodoDate = localStorage.getItem('currentTodoDate');
+		$('#edit_form input[name=todo_name_edit]',this).val(currentTodoName);
+		$('#edit_form input[name=todo_date_edit]',this).val(currentTodoDate);
+	});
+	
+	$(document).on('pageshow','#home',function(){
+		window.location.reload();
+	});
 
-        $(document).on('pageshow', '#edit', function () {
-            currentTodoName = localStorage.getItem('currentTodoName');
-            currentTodoDate = localStorage.getItem('currentTodoDate');
-            $('#edit_form input[name=todo_name]', this).val(currentTodoName);
-            $('#edit_form input[name=todo_date]', this).val(currentTodoDate);
-        });
-
-        $(document).on('pageshow', '#home', function () {
-            window.location.reload();
-        });
-
-        //Clear Todos
-        $('#clear_btn').click(function () {
-            localStorage.clear();
+	//Clear Todos
+	$('#clear_btn').click(function(){
+		localStorage.clear();
         });
     });
